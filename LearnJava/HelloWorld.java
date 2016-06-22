e3c96fe83d3b94f39570ddbaad515c77c9e714c2
Tony Nguyen <tonyfreelance@users.noreply.github.com>
2016-06-22 10:56:26 +0700

    Created by Coding Ground Client

---
+++ LearnJava/HelloWorld.java
@@ -0,0 +1,9 @@
+public class VariableTest {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        System.out.println("The value of i is: " + i);
+        i = i + 24;
+        System.out.println("The value of i is now: " + i);
+    }
+}
